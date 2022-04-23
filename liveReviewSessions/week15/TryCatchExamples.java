0dba5a773e32987e6de3665cfea8409e1671214e
trnyc <turanyuce@gmail.com>
2022-04-23 18:18:41 +0300

    class 2 and 3

---
+++ liveReviewSessions/week15/TryCatchExamples.java
@@ -0,0 +1,16 @@
+package week15;
+
+public class TryCatchExamples {
+    public static void main(String[] args) {
+
+        try {
+            System.out.println(10 / 2);
+            System.out.println(10 / 0);
+            System.out.println(10 / 3); // this line is skipped because of exception on previous line
+        }catch (ArithmeticException arithmeticException){
+            System.out.println("Arithmetic exception handled");
+            //System.out.println(arithmeticException.getMessage());
+        }
+
+    }
+}
