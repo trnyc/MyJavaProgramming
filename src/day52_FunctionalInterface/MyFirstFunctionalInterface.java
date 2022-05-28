9d541aaf7bb47d774082f72fd2d79c14b0705dbe
trnyc <turanyuce@gmail.com>
2022-05-28 09:49:17 +0300

    day52 functional interface

---
+++ src/day52_FunctionalInterface/MyFirstFunctionalInterface.java
@@ -0,0 +1,8 @@
+package day52_FunctionalInterface;
+
+@FunctionalInterface
+public interface MyFirstFunctionalInterface {
+
+    void apply(int n); // this is an abstract method by default
+
+}
---
+++ src/day52_FunctionalInterface/MySecondFunctionalInterface.java
@@ -0,0 +1,9 @@
+package day52_FunctionalInterface;
+
+
+@FunctionalInterface
+public interface MySecondFunctionalInterface<T>{ // T stands for generic type, it accepts any object type
+
+    void test(T data);
+
+}
